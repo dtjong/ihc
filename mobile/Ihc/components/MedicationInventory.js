0830982e2174282b06ec328bd2abe9627997d00c
kenny <wwyi@ucsd.edu>
2018-10-22 23:01:39 -0700

    connected backend api for medication inventory screen

--- mobile/Ihc/components/MedicationInventory.js
+++ mobile/Ihc/components/MedicationInventory.js
@@ -4,68 +4,43 @@ import {
   Text,
   View
 } from 'react-native';
+
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import UpdateMedicationModal from './UpdateMedicationModal';
 import Button from './Button';
-import Medication from '../models/Medication';
 
 export default class MedicationInventory extends Component<{}> {
   /*
    * Expects in props:
    *  {
    *    rows: [Medication],
-
-   *    saveModal: function
+   *    createMedication: function,
+   *    updateMedication: function
    *  }
    */
   constructor(props) {
     super(props);
     this.tableHeaders = ['Drug Name', 'Quantity', 'Dosage', 'Units', 'Notes'];
     this.rowNum = 0;
-
-    // showModal is the modal to update medication
-    // medicationToEdit is the medication that will be edited
-    this.state = {showModal: false, 
-      medicationToEdit: {
-        drugName: '',
-        quantity: 0,
-        dosage: 0,
-        units: '',
-        comments: '' //Consider keeping track of multiple comments (array of strings)
-      }
-    };
+    this.state = { showModal: false, oldKey: null};
   }
 
-
-  openEditModal = (medicationToEdit) => {
-    this.setState({medicationToEdit: medicationToEdit, showModal: true});
-    });
+  openEditModal = (oldMedication) => {
+    const oldKey = oldMedication.key;
+    this.setState({ showModal: true, oldKey: oldKey});
   }
   openAddModal = () => {
-    this.setState({showModal: true, 
-      medicationToEdit: {
-        drugName: '',
-        quantity: 0,
-        dosage: 0,
-        units: '',
-        comments: '' 
-      }
-    });
+    this.setState({ showModal: true, oldKey: null });
   }
   closeModal = () => {
-    this.setState({showModal: false, 
-      medicationToEdit: {
-        drugName: '',
-        quantity: 0,
-        dosage: 0,
-        units: '',
-        comments: '' 
-      }
-    });
+    this.setState({ showModal: false });
   }
-
-  updateMedication = (newMedication) => {
-    this.setState({medicationToEdit: newMedication});
+  saveModal = (newMedication) => {
+    if (this.state.oldKey == null) {
+      this.props.createMedication(newMedication);
+    } else {
+      this.props.updateMedication(this.state.oldKey, newMedication);
+    }
   }
 
   // Renders each column in a row
@@ -89,13 +64,13 @@ export default class MedicationInventory extends Component<{}> {
 
   renderRow = (medication, keyFn) => {
     //puts the properties of medication into an array
-    let medData = this.extractMedicationElements(medication);    
+    let medData = this.extractMedicationElements(medication);
 
     // Renders each property
     let cols = medData.map( (e,i) => {
       return this.renderCol(e,keyFn,i);
     });
-    
+
     return (
       // Entire row is clickable to open a modal to edit
       <Row key={`row${this.rowNum++}`} style={styles.rowContainer}
@@ -127,9 +102,7 @@ export default class MedicationInventory extends Component<{}> {
         <UpdateMedicationModal
           showModal={this.state.showModal}
           closeModal={this.closeModal}
-          saveModal={() => this.props.saveModal(this.state.medicationToEdit)}
-          updateMedication={this.updateMedication}
-          medicationToEdit={this.state.medicationToEdit}
+          saveModal={this.saveModal}
         />
 
         <Button style={styles.buttonContainer}
