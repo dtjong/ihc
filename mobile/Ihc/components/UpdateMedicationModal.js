a35b971ba78c815903ac1ad0eb8f8075f0aefcd8
kenny <wwyi@ucsd.edu>
2019-01-19 18:26:23 -0800

    revert weird commit

--- mobile/Ihc/components/UpdateMedicationModal.js
+++ mobile/Ihc/components/UpdateMedicationModal.js
@@ -24,7 +24,6 @@ export default class UpdateMedicationModal extends Component<{}> {
    */
   constructor(props) {
     super(props);
-    this.state = {formValues: {} };
   }
 
   onFormChange = (value) => {
